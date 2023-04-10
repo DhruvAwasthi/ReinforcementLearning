e5a5f6e05c06a268cfb67fc4e70bb6920e9b218d
Dhruv Awasthi <dhruvawasthicc@gmail.com>
2023-04-10 17:07:43 +0530

    complete code for off policy MC control

--- RaceTrack Example/race_track.py
+++ RaceTrack Example/race_track.py
@@ -149,11 +149,12 @@ class Env:
         reward = -1
         self.data.episode["A"].append(action)
         if self.is_finish_line_crossed(state, action):
+            reward = 1
             new_state = self.get_new_state(state, action)
             self.data.episode["R"].append(reward)
             self.data.episode["S"].append(new_state)
             self.step_count += 1
-            return None, new_state
+            return reward, new_state
 
         elif self.is_out_of_track(state, action):
             new_state = self.start()
@@ -259,6 +260,7 @@ class Visualizer:
             if ret is not None:
                 return ret
 
+
 # generate racetrack
 RaceTrackObj = RaceTrack()
 race_track = RaceTrackObj.generate_racetrack()
@@ -267,3 +269,119 @@ print(race_track)
 
 # visualize racetrack
 RaceTrackObj.visualize_racetrack()
+
+# initialize environment
+env = Env()
+# initialize agent
+agent = Agent()
+
+
+class OffPolicyMonteCarloControl:
+    def __init__(self, data):
+        """Intitialize for all states, actions, etc."""
+        self.data = data
+        for i in range(26):
+            for j in range(13):
+                if self.data.racetrack[i, j] != -1:
+                    for k in range(6):
+                        for l in range(6):
+                            self.data.policy[i, j, k, l] = np.argmax(self.data.Q_vals[i, j, k, l])
+
+    def determine_probability_behaviour(self, state, action, possible_actions):
+        best_action = self.data.policy[tuple(state)]
+        num_actions = len(possible_actions)
+
+        if best_action in possible_actions:
+            if action == best_action:
+                prob = 1 - self.data.epsilon + self.data.epsilon / num_actions
+            else:
+                prob = self.data.epsilon / num_actions
+        else:
+            prob = 1 / num_actions
+
+        self.data.episode["probs"].append(prob)
+
+    def generate_target_policy_action(self, state, possible_actions):
+        """Returns target policy action; takes state and return an action
+        using this policy.
+        """
+        if self.data.policy[tuple(state)] in possible_actions:
+            action = self.data.policy[tuple(state)]
+        else:
+            action = np.random.choice(possible_actions)
+
+        self.determine_probability_behaviour(state, action, possible_actions)
+
+        return action
+
+    def generate_behavioural_policy_action(self, state, possible_actions):
+        """Returns behavioural policy action which would be epsilon-greedy pi policy;
+        takes state and returns an action using this epsilon-greedy pi policy."""
+        if np.random.rand() > self.data.epsilon and self.data.policy[tuple(state)] in possible_actions:
+            action = self.data.policy[tuple(state)]
+        else:
+            action = np.random.choice(possible_actions)
+
+        self.determine_probability_behaviour(state, action, possible_actions)
+
+        return action
+
+    def evaluate_target_policy(self):
+        env.reset()
+        state = env.start()
+        self.data.episode["S"].append(state)
+        rew = -1
+        while rew != 1:
+            action = agent.get_action(state, self.generate_target_policy_action)
+            rew, state = env.step(state, action)
+
+        self.data.rewards.append(sum(self.data.episode["R"][1:]))
+
+    def save_your_work(self):
+        self.data.save_Q_vals()
+        self.data.save_C_vals()
+        self.data.save_policy()
+        self.data.save_rewards()
+
+    def control(self, env, agent):
+        """Performs MC control using episode list"""
+        env.reset()
+        state= env.start()
+        self.data.episode["S"].append(state)
+        rew = - 1
+        while rew != 1:
+            action = agent.get_action(state, self.generate_behavioural_policy_action)
+            rew, state = env.step(state, action)
+
+        G = 0
+        W = 1
+        T = env.step_count
+
+        for t in range(T - 1, -1, -1):
+            G = data.gamma * G + self.data.episode["R"][t+1]
+            S_t = tuple(self.data.episode["S"][t])
+            A_t = agent.map_to_one_dimension(self.data.episode["A"][t])
+
+            S_list = list(S_t)
+            S_list.append(A_t)
+            SA = tuple(S_list)
+
+            self.data.C_vals[SA] += W
+            self.data.Q_vals[SA] += (W * (G - self.data.Q_vals[SA])) / (self.data.C_vals[SA])
+            self.data.policy[S_t] = np.argmax(self.data.Q_vals[S_t])
+            if A_t != self.data.policy[S_t]:
+                break
+            W /= self.data.episode["probs"][t]
+
+    def plot_rewards(self):
+        ax, fig = plt.subplots(figsize=(30, 15))
+        x = np.arange(1, len(self.data.rewards) + 1)
+        plt.plot(x * 10, self.data.rewards, linewidth=0.5, color="#BB8FCE")
+        plt.xlabel("Episode number", size=20)
+        plt.ylabel("Reward", size=20)
+        plt.title("Plot of Reward vs Episode Number", size=20)
+        plt.xticks(size=20)
+        plt.yticks(size=20)
+        plt.savefig("Reward_Graph.png")
+        plt.close()
+
